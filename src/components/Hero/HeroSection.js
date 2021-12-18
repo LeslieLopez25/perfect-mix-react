a80b87ec0fd2882078bedb5694462ccd313fe4ce
Leslie Lopez <leslie2515lopez@gmail.com>
2021-12-17 21:56:36 -0600

    Updated the hero section

--- src/components/Hero/HeroSection.js
+++ src/components/Hero/HeroSection.js
@@ -7,20 +7,18 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <div className="left">
-        <h1>ADVENTURE AWAITS</h1>
+        <h1>Food from scratch with love from our kitchens</h1>
         <p>What are you waiting for?</p>
         <div className="hero-btns">
           <Button
             className="btns"
-            buttonStyle="btn--outline"
+            buttonStyle="btn--primary"
             buttonSize="btn--large"
           >
-            GET STARTED
+            Place Order
           </Button>
         </div>
-        <div className="right">
-          <div className="hero-img"></div>
-        </div>
+        <div className="right"></div>
       </div>
     </div>
   );
