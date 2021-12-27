a2888317a3a70ea467c253fbff6e6ac503a02d1a
Leslie Lopez <leslie2515lopez@gmail.com>
2021-12-26 22:43:08 -0600

    Completed the specials section

--- src/components/SpecialsSection/SpecialArea2.js
+++ src/components/SpecialsSection/SpecialArea2.js
@@ -1,21 +1,57 @@
 import React from "react";
 import "../../App.css";
 import { Button } from "../Button";
+import Img3 from "../../images/appetizers-special.jpg";
+import Img4 from "../../images/drink-special.jpg";
 import "../SpecialsSection/SpecialArea1.css";
 
 function Specials2Section() {
   return (
     <div className="specials-container">
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
-      <div className="specials-btns">
-        <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-        >
-          GET STARTED
-        </Button>
+      <h2 className="specials-heading">
+        Specials This Week For The Menu And Desserts
+      </h2>
+      <div className="specials-wrapper">
+        <div className="specials-card">
+          <img className="specials-img" src={Img3} alt="Tamales" />
+          <div className="specials-info">
+            <h3 className="specials-name">Tamales</h3>
+            <p className="description">
+              Pizza ipsum dolor amet lasagna sauteed onions hawaiian, bbq rib
+              chicken and bacon peppers chicken wing.
+            </p>
+            <p className="price">$1.50</p>
+            <div className="specials-btns">
+              <Button
+                className="btns"
+                buttonStyle="btn--primary"
+                buttonSize="btn--large"
+              >
+                Add To Cart
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="specials-card">
+          <img className="specials-img" src={Img4} alt="churros" />
+          <div className="specials-info">
+            <h3 className="specials-name">Red Wine</h3>
+            <p className="description">
+              Pizza ipsum dolor amet lasagna sauteed onions hawaiian, bbq rib
+              chicken and bacon peppers chicken wing.
+            </p>
+            <p className="price">$9.99</p>
+            <div className="specials-btns">
+              <Button
+                className="btns"
+                buttonStyle="btn--primary"
+                buttonSize="btn--large"
+              >
+                Add To Cart
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/SpecialsSection/SpecialsArea1.js
+++ src/components/SpecialsSection/SpecialsArea1.js
@@ -1,41 +1,55 @@
 import React from "react";
 import "../../App.css";
 import { Button } from "../Button";
+import Img1 from "../../images/menu-special.jpg";
+import Img2 from "../../images/dessert-special.jpg";
 import "../SpecialsSection/SpecialArea1.css";
 
 function SpecialsSection() {
   return (
     <div className="specials-container">
-      <h1>Specials This Week For The Menu And Desserts</h1>
+      <h2 className="specials-heading">
+        Specials This Week For The Menu And Desserts
+      </h2>
       <div className="specials-wrapper">
-        <div className="specials-img menu">
-          <h2 className="product-name">tacos de birria</h2>
-          <p className="description">What are you waiting for?</p>
-          <p className="price">$5.99</p>
-          <div className="specials-btns">
-            <Button
-              className="btns"
-              buttonStyle="btn--primary"
-              buttonSize="btn--large"
-            >
-              Add To Cart
-            </Button>
+        <div className="specials-card">
+          <img className="specials-img" src={Img1} alt="tacos-de-birria" />
+          <div className="specials-info">
+            <h3 className="specials-name">Tacos de Birria</h3>
+            <p className="description">
+              Pizza ipsum dolor amet lasagna sauteed onions hawaiian, bbq rib
+              chicken and bacon peppers chicken wing.
+            </p>
+            <p className="price">$5.99</p>
+            <div className="specials-btns">
+              <Button
+                className="btns"
+                buttonStyle="btn--primary"
+                buttonSize="btn--large"
+              >
+                Add To Cart
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="specials-wrapper">
-        <div className="specials-img dessert">
-          <h3 className="product-name">tacos de birria</h3>
-          <p className="description">What are you waiting for?</p>
-          <p className="price">$5.99</p>
-          <div className="specials-btns">
-            <Button
-              className="btns"
-              buttonStyle="btn--primary"
-              buttonSize="btn--large"
-            >
-              Add To Cart
-            </Button>
+        <div className="specials-card">
+          <img className="specials-img" src={Img2} alt="churros" />
+          <div className="specials-info">
+            <h3 className="specials-name">Churros</h3>
+            <p className="description">
+              Pizza ipsum dolor amet lasagna sauteed onions hawaiian, bbq rib
+              chicken and bacon peppers chicken wing.
+            </p>
+            <p className="price">$2.99</p>
+            <div className="specials-btns">
+              <Button
+                className="btns"
+                buttonStyle="btn--primary"
+                buttonSize="btn--large"
+              >
+                Add To Cart
+              </Button>
+            </div>
           </div>
         </div>
       </div>
