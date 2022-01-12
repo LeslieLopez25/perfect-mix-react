286c736b35b9993b62fee9dbe2c934678e6a55a1
Leslie Lopez <leslie2515lopez@gmail.com>
2022-01-11 21:27:50 -0600

    Revert "Added Product folder"
    This reverts commit 5f04e14510d64c5d286db126e5c6af450be51fff.

--- src/components/pages/Cart/Cart.js
+++ src/components/pages/Cart/Cart.js
@@ -2,9 +2,5 @@ import React from "react";
 import "../../../App.css";
 
 export default function Cart() {
-  return (
-    <div className="specials-container">
-      <h1 className="heading">Your Cart</h1>
-    </div>
-  );
+  return <h1 className="cart">Cart</h1>;
 }
