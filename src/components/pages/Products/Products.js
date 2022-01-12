439191ef4faaa3bac1f6e24e1aec8f3f7e06b600
Leslie Lopez <leslie2515lopez@gmail.com>
2022-01-11 21:25:53 -0600

    Revert "Finished the data section for the products"
    This reverts commit d048ed2e8226fb2a6a3f554838fa7a6fcebff1d7.

--- src/components/pages/Products/Products.js
+++ src/components/pages/Products/Products.js
@@ -9,8 +9,10 @@ export default function Products(props) {
     <div>
       <img className="small" src="{product.image}" alt="{product.name}" />
       <h3>{product.name}</h3>
-      <div>{product.desc}</div>
       <div>${product.price}</div>
+      <div>
+        <button>Add To Cart</button>
+      </div>
     </div>
   );
 }
