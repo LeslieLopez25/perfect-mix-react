9450e34626920cf4f70f37e75c499e7bef0f4447
Leslie Lopez <leslie2515lopez@gmail.com>
2022-04-07 19:33:40 -0500

    Added comments for product card file

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
 import "../Products/ProductElements.css";
 
-export default function Product( {item} ) {
+export default function Product({ item }) {
+  // Created a function to handle the main properties of the products
   const { img, title, desc, price } = item;
   
+  // To have the page start at the top of the page
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
+    // The main product card
     <>
       <div className="productLineUp">
         <img src={img} alt={title} />
