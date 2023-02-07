f6e52e9b04d44958167ffc7fb5ed6928a6eb45ae
Leslie Lopez <leslie2515lopez@gmail.com>
2023-02-06 23:54:14 -0600

    Updated names for navbar component

--- src/components/Navbar/navbar.component.js
+++ src/components/Navbar/navbar.component.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "../Button/Button";
+import { Button } from "../Button/button.component";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./navbar.styles.css";
 
 export default function Navbar() {
   const [click, setClick] = useState(false);
