79102835d1a9b30213f6e1a1614028b29be86413
Leslie Lopez <leslie2515lopez@gmail.com>
2023-02-09 01:08:15 -0600

    Added shop context folder

--- src/App.js
+++ src/App.js
@@ -12,23 +12,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./components/pages/Gallery/gallery.component";
 import Cart from "./components/Cart/cart.component";
 import Footer from "./components/Footer/footer.component";
+import { ShopContextProvider } from "./components/context/shop-context";
 
 export default function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/beverages" element={<Beverages />} />
-          <Route path="/desserts" element={<Desserts />} />
-          <Route path="/appetizers" element={<Appetizers />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/gallery" element={<Gallery />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/beverages" element={<Beverages />} />
+            <Route path="/desserts" element={<Desserts />} />
+            <Route path="/appetizers" element={<Appetizers />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/gallery" element={<Gallery />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </ShopContextProvider>
     </>
   );
 }
--- src/components/Products/product-card.component.js
+++ src/components/Products/product-card.component.js
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
+import { Button } from "../Button/button.component";
+import { ShopContext } from "../context/shop-context";
 import "../Products/product.styles.css";
 
 export default function Product({ item }) {
   // Created a function to handle the main properties of the products
-  const { img, title, desc, price } = item;
+  const { id, img, title, desc, price } = item;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemAmount = cartItems[id];
 
   // To have the page start at the top of the page
   useEffect(() => {
@@ -18,6 +23,14 @@ export default function Product({ item }) {
         <h3> {title} </h3>
         <p> {desc} </p>
         <h4> ${price} </h4>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          onClick={() => addToCart(id)}
+        >
+          Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}
+        </Button>
       </div>
     </>
   );
