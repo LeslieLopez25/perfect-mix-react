98ebfd3708576dba852f9b640cc143f6aa6ef79a
Leslie Lopez <leslie2515lopez@gmail.com>
2024-09-20 00:18:30 -0600

    Added auth0 for authentication

---
+++ client/auth/Auth0Provider.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const AuthProvider = ({ children }) => {
+  return (
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      redirectUri={`${window.location.origin}/cart`}
+      audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default AuthProvider;
