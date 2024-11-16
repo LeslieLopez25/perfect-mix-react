444cb849ecd8c589351b2317e6fbc20f480ae410
Leslie Lopez <leslie2515lopez@gmail.com>
2024-11-15 23:17:31 -0600

    Updated index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import AuthProvider from "./auth/Auth0Provider";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
