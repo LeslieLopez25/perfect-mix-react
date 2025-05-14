cc8199681e2b0fd83440f7c2a6c04ab7118da7a8
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-13 23:35:50 -0600

    Added comments to export menu section component

--- client/src/components/export-menu-section.component.js
+++ client/src/components/export-menu-section.component.js
@@ -1,6 +1,10 @@
-// Created export main pages
+// Export the Home component from the home page section
 export { default as Home } from "../pages/MenuSections/home.component";
+// Export the Main component from the main dishes section
 export { default as Main } from "../pages/MenuSections/main.component";
+// Export the Beverages component from the beverages section
 export { default as Beverages } from "../pages/MenuSections/beverages.component";
+// Export the Desserts component from the desserts section
 export { default as Desserts } from "../pages/MenuSections/desserts.component";
+// Export the Appetizers component from the appetizers section
 export { default as Appetizers } from "../pages/MenuSections/appetizers.component";
