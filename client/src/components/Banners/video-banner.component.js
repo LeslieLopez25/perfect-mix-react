c6088ebf950d0103d87b1783965410b43629fc58
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-13 21:15:52 -0600

    Added comments to video banner component

--- client/src/components/Banners/video-banner.component.js
+++ client/src/components/Banners/video-banner.component.js
@@ -3,6 +3,8 @@ import Video from "../../assets/video/video.mp4";
 
 import "../Banners/banners.styles.css";
 
+// VideoBanner renders a video element that plays automatically,
+// loops continuously, and is muted
 export default function VideoBanner() {
   return (
     <div className="video-container">
