043b6d3eeb360b49775c9f8f3b00917017b2bc99
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-13 23:32:38 -0600

    Added comments to export banner section component

--- client/src/components/export-banner-section.component.js
+++ client/src/components/export-banner-section.component.js
@@ -1,3 +1,4 @@
-// Created export banner section
+// Export the default Banner component from banner.component.js
 export { default as Banner } from "./Banners/banner.component";
+// Export the default Video component from video-banner.component.js
 export { default as Video } from "./Banners/video-banner.component";
