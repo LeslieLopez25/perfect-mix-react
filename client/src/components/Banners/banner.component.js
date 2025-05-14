22490a8d696e70757e19f62e5df9c124f294bc26
Leslie Lopez <leslie2515lopez@gmail.com>
2025-05-13 21:12:39 -0600

    Added comments to banner component

--- client/src/components/Banners/banner.component.js
+++ client/src/components/Banners/banner.component.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import "../Banners/banners.styles.css";
 
+// Banners is a presentational component that renders a styled banner container
+// The actual appearance and content are handled by CSS in 'banner.styles.css
 export default function Banners() {
   return <div className="banner-container"></div>;
 }
