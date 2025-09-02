946ee5a122418eb342b6ed27d14eee42252fb3db
Leslie Lopez <leslie2515lopez@gmail.com>
2025-09-01 23:57:11 -0600

    Finished setting up home.spec.js test

--- e2e-tests/tests/home.spec.js
+++ e2e-tests/tests/home.spec.js
@@ -3,7 +3,8 @@ import { test, expect } from "@playwright/test";
 test("should stay logged in with save state", async ({ page }) => {
   await page.goto("http://localhost:3000/");
 
-  await expect(page.getByText(/welcome/i)).toBeVisible();
+  const email = process.env.TEST_AUTH0_EMAIL;
+  await expect(page.locator("nav .nav-email")).toHaveText(email);
 
   await expect(page.getByText(/testperfectmix@test.com/i)).toBeVisible();
 });
